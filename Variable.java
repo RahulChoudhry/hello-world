46b596735e80c0bf1b561691cb257f2d31e9b18d
RahulChoudhry <98380796+RahulChoudhry@users.noreply.github.com>
2022-03-30 08:44:37 +0530

    initial commit

---
+++ Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    public static void main(String[] args) {
+        int passengers = 5;
+        System.out.println("Passengers is = " + passengers);
+
+        int busTickets = 5;
+        System.out.println("BusTickets is = " + busTickets);
+    }
+}
